cbaf32d7b5aa7084fa99ae3dac389a2399569274
Sirisak Lueangsaksri <spywhere@me.com>
2017-04-05 09:34:19 +0700

    Upgrade the API to level 2 on the method

--- app.ts
+++ app.ts
@@ -18,59 +18,63 @@ app.get("/", (request, response) => {
     response.send("API is working.");
 });
 
-app.post("/movies", (request, response) => {
-    switch (request.body.action) {
-        case "viewList":
-            db.movies.findAsync({})
-                .then((results) => {
-                    response.json(results);
-                })
-                .catch((error) => {
-                    response.json({ error: error });
-                });
-            break;
-        case "addNew":
-            db.movies.insertAsync({
-                title: request.body.title
-            }).then((document) => {
-                response.json(document);
-            }).catch((error) => {
-                response.json({ error: error });
-            });
-            break;
-        default:
-            response.json({ error: { message: "No action given" } });
-    }
+app.get("/movies", (request, response) => {
+    db.movies.findAsync({})
+        .then((results) => {
+            response.json(results);
+        })
+        .catch((error) => {
+            response.json({ error: error });
+        });
 });
 
-app.post("/movies/:id", (request, response) => {
-    switch (request.body.action) {
-        case "view":
-            db.movies.findOneAsync({
-                _id: request.params.id
-            }).then((document) => {
-                response.json(document);
-            }).catch((error) => {
-                response.json({ error: error });
-            });
-            break;
-        case "rate":
-            db.movies.updateAsync(
-                { title: request.params.id },
-                { $set: { rating: request.body.rating } }
-            ).then((result) => {
-                response.json({
-                    success: {
-                        message: `${ result.numberOfUpdated } records updated`
-                    }
-                });
-            }).catch((error) => {
-                response.json({ error: error });
-            });
-            break;
-        default:
-            response.json({ error: { message: "No action given" } });
-    }
+app.post("/movies", (request, response) => {
+    db.movies.insertAsync({
+        title: request.body.title
+    }).then((document) => {
+        response.json(document);
+    }).catch((error) => {
+        response.json({ error: error });
+    });
+});
+
+app.get("/movies/:id", (request, response) => {
+    db.movies.findOneAsync({
+        _id: request.params.id
+    }).then((document) => {
+        response.json(document);
+    }).catch((error) => {
+        response.json({ error: error });
+    });
+});
+
+app.put("/movies/:id", (request, response) => {
+    db.movies.updateAsync(
+        { title: request.params.id },
+        { $set: { rating: request.body.rating } }
+    ).then((result) => {
+        response.json({
+            success: {
+                message: `${ result.numberOfUpdated } records updated`
+            }
+        });
+    }).catch((error) => {
+        response.json({ error: error });
+    });
+});
+
+app.delete("/movies/:id", (request, response) => {
+    db.movies.removeAsync(
+        { _id: request.params.id }
+    ).then((numberOfDeleted) => {
+        response.json({
+            success: {
+                message: `${ numberOfDeleted } records updated`
+            }
+        });
+    }).catch((error) => {
+        response.json({ error: error });
+    });
 });
 
 app.listen(3000, () => {
